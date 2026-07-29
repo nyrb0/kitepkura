3e863937857bdc0284ce67e74f09d91018015b36
nurbo <nubolotkadyrov06@gmail.com>
2026-07-29 17:01:07 +0600

    update proxy

--- frontend/src/proxy.ts
+++ frontend/src/proxy.ts
@@ -36,5 +36,5 @@ export function proxy(request: NextRequest) {
 }
 
 export const config = {
-    matcher: ['/((?!_next|favicon.ico|logo.png|.*\\..*).*)'],
+    matcher: ['/admin/:path*', '/kg/:path*', '/ru/:path*', '/'],
 };
